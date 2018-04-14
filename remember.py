9791f3551d8a2268784fb950bade3ae92f18c438
Drazen <drazenzen@gmail.com>
2018-04-14 11:29:04 +0200

    Switched to MIT licence.

--- remember.py
+++ remember.py
@@ -3,23 +3,6 @@
 #
 #  remember.py
 #
-#  Copyright 2018 Drazen <drazenzen@gmail.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#
 
 import os
 import pickle
@@ -39,7 +22,7 @@ class RememberPlugin(geany.Plugin):
     __plugin_name__ = "Remember"
     __plugin_version__ = "0.02"
     __plugin_description__ = "Remember folds and bookmarks between sessions."
-    __plugin_author__ = "Drazen <drazenzen@gmail.com>"
+    __plugin_author__ = "Drazen <drazen@k2net.hr>"
 
     # Scintilla.h
     # https://sourceforge.net/p/scintilla/code/ci/default/tree/include/Scintilla.h
